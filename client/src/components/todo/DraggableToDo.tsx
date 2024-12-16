1e793b57cd49a47aaeca74298afe98fb2dd706c8
nunuzzang <sivwo24@naver.com>
2024-12-16 15:56:32 +0900

    ToDo 삭제기능 추가

--- client/src/components/todo/DraggableToDo.tsx
+++ client/src/components/todo/DraggableToDo.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "../../atoms";
 
 const ToDoCard = styled.div`
-    width: 85%;
+    width: 100%;
     height: 40px;
     margin: 4px 0;
     border-radius: 12px;
-    background-color: #bbaef5;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding-left: 10px;
-    color: white;
     grid-column: 1 span 1;
     grid-row: 2 span 1;
+    background-color: #bbaef5;
+    padding-left: 10px;
+    color: white;
+    display: flex;
+    align-items: center;
+    display: grid;
+    grid-template-columns: 20px auto 35px 35px;
 `;
 
 interface IDragabbleCardProps {
@@ -24,6 +27,18 @@ interface IDragabbleCardProps {
 }
 
 function DraggableToDo({ toDoId, toDoText, index }: IDragabbleCardProps) {
+    const setTodoState = useSetRecoilState(toDoState);
+    const onDelete = () => {
+        setTodoState((prevTodos) => {
+            const updatedTodos = { ...prevTodos };
+            const categories = Object.keys(updatedTodos);
+            for (let i = 0; i < (categories.length); i++) {
+                // "To Do", "Doing", "Done" 중 해당 ID를 찾아서 삭제
+                updatedTodos[categories[i]] = updatedTodos[categories[i]].filter((todo) => todo.id !== toDoId);
+            }
+            return updatedTodos;
+        });
+    };
     return (
         <Draggable draggableId={toDoId + ""} index={index}>
             {(provided) => (
@@ -32,7 +47,10 @@ function DraggableToDo({ toDoId, toDoText, index }: IDragabbleCardProps) {
                     {...provided.draggableProps} // 드래그 가능한 속성
                     {...provided.dragHandleProps} // 드래그 핸들 속성
                 >
+                    <input type="checkbox"></input>
                     {toDoText}
+                    <button>수정</button>
+                    <button onClick={onDelete}>삭제</button>
                 </ToDoCard>
             )}
         </Draggable>

bdd883f815a25b319c8ea1722cf67bbd20a8c644
AHNBelieve <an0731an@cau.ac.kr>
2024-12-23 16:47:02 +0900

    좀 비효율적이지만 클라 - 서버 - Supabase연결 성공 및 유저 별 데이터 저장 성공 하지만 테스트 필요

--- client/src/components/LoginComponent/LoginComponent.tsx
+++ client/src/components/LoginComponent/LoginComponent.tsx
@@ -1,6 +1,6 @@
 // src/components/LoginComponent.tsx
 import React, { useEffect, useState } from "react";
-import { checkSession, supabase } from "../../supabase"; // Supabase 클라이언트 임포트
+import { supabase } from "../../supabase"; // Supabase 클라이언트 임포트
 
 const LoginComponent: React.FC = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -40,10 +40,40 @@ const LoginComponent: React.FC = () => {
     func();
     setIsLoading(false);
   }, []);
-  if (isLoading || isLogin) {
-    return;
-  }
 
+  const testHandler = async () => {
+    try {
+      const token = await supabase.auth
+        .getSession()
+        .then(({ data: { session } }) => session?.access_token);
+      if (!token) {
+        alert("로그인 해주세요");
+        return;
+      }
+
+      const response = await fetch("http://localhost:3000/DailyData", {
+        headers: {
+          Authorization: `Bearer ${token}`, // JWT를 Authorization 헤더에 추가
+        },
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json(); // 에러 응답을 JSON으로 파싱
+        throw new Error(
+          errorData.error || `HTTP error! status: ${response.status}`
+        );
+      }
+
+      const data = await response.json();
+      console.log("Products:", data);
+    } catch (error: any) {
+      console.error("Error fetching products:", error.message);
+      alert("데이터를 가져오는 중 오류가 발생했습니다: " + error.message);
+    }
+  };
+  if (isLoading || isLogin) {
+    return <button onClick={testHandler}>테스트</button>;
+  }
   return (
     <div className="login-container">
       <div className="login-box">
--- server/src/daily-data/daily-data.controller.ts
+++ server/src/daily-data/daily-data.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { DailyDataService } from './daily-data.service';
 
-@Controller('daily-data')
-export class DailyDataController {}
+@Controller('DailyData')
+export class DailyDataController {
+  constructor(private readonly DailyDataService: DailyDataService) {}
+
+  @Get()
+  async getDailyData(@Headers('Authorization') authHeader: string) {
+    return await this.DailyDataService.getDailyData(authHeader);
+  }
+}
